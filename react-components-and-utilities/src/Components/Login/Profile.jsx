b511d68ef568d1c19e40187f71fc8fa28fad07b6
Juan Gustin <juandgustin@gmail.com>
2023-06-24 13:38:10 -0500

    Button 2

--- react-components-and-utilities/src/Components/Login/Profile.jsx
+++ react-components-and-utilities/src/Components/Login/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
-    return <div>Loading ...</div>;
+    return <div>Cargando...</div>;
   }
 
   let allUserData = user;
